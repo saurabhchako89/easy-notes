23c760aa8a015992c7afba229f026e16b53f3a99
saurabhchako89 <saurabhchako89@gmail.com>
2018-12-11 00:30:19 -0600

    Initial commit

---
+++ src/main/java/com/poc/easynotes/repository/NoteRepository.java
@@ -0,0 +1,9 @@
+package com.poc.easynotes.repository;
+
+import com.poc.easynotes.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+}
